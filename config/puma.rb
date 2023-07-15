698fea845b289d6d42ef8139f227699805db1886
mugihouse <mugi.stnk100122@gmail.com>
2023-07-16 00:16:13 +0900

    Fix: SSL証明書をdevelopment環境でのみ適用

--- config/puma.rb
+++ config/puma.rb
@@ -16,10 +16,12 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # port ENV.fetch("PORT") { 3000 }
-ssl_bind "0.0.0.0", "3000", {
-  cert: "config/certs/localhost.pem",
-  key:  "config/certs/localhost-key.pem"
+if ENV.fetch('RAILS_ENV') { 'development' } == 'development'
+  ssl_bind '0.0.0.0', '3000', {
+    cert: 'config/certs/localhost.pem',
+    key: 'config/certs/localhost-key.pem'
 }
+end
 
 # Specifies the `environment` that Puma will run in.
 #
