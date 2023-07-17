ef40507b3e1a9946318912a667e82d08bbab9a82
mugihouse <mugi.stnk100122@gmail.com>
2023-07-17 17:31:40 +0900

    Add: ログインのためのヘルパーメソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :login_required
+  helper_method :logged_in?
+
   def index
     render html: "hello!"
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to root_path unless current_user
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
