64277bdb69fcd391e4d2f65cc8fa66241edf2094
mugihouse <mugi.stnk100122@gmail.com>
2023-07-24 23:57:41 +0900

    Add: new,createアクションの内容を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
-  def new; end
+  require 'net/http'
+  require 'uri'
 
-  def create; end
+  before_action :set_liff_id, only: %i[new]
+
+  def new
+    if current_user
+      redirect_to after_login_path
+    end
+  end
+
+  def create
+    id_token = params[:idToken]
+    channel_id = ENV['CHANNEL_ID']
+    # IDトークンを検証し、ユーザーの情報を取得
+    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
+    # LINEユーザーIDを取得
+    line_id = JSON.parse(res.body)['sub']
+    user = User.find_by(line_id: line_id)
+    if user.nil?
+      User.create(line_id: line_id)
+    elsif (session[:user_id] = user.id)
+      render json: user
+    end
+  end
 end
