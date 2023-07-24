d88ea36f0006b92b902f8c753f1eda1a20751487
mugihouse <mugi.stnk100122@gmail.com>
2023-07-24 23:58:34 +0900

    Change: turboの読み込み設定を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "./controllers"
 import * as bootstrap from "bootstrap"
