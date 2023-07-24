1a3824aa2a125b462a85fab3589b6b687c6c7587
mugihouse <mugi.stnk100122@gmail.com>
2023-07-24 23:40:00 +0900

    Delete: nameカラムのバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :tasks, dependent: :destroy
 
-  validates :name, presence: true
   validates :line_id, presence: true
 end
