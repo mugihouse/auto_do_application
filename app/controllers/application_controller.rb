8e8a9ce089ebeb5f21b5673781a4b81d23b08782
mugihouse <mugi.stnk100122@gmail.com>
2023-07-24 23:51:15 +0900

    Add: set_liff_idメソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def set_liff_id
+    gon.liff_id = ENV['LIFF_ID']
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
