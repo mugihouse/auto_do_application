9a44b5bd843daed72fef1571d9616fa06a47d446
mugihouse <mugi.stnk100122@gmail.com>
2023-08-04 16:04:59 +0900

    Add: create, showアクション追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,29 @@
 class ProfilesController < ApplicationController
-  before_action :set_user, only: %i[new]
+  before_action :set_profile, only: %i[show]
 
   def new
     @profile = Profile.new()
   end
 
   def create
-
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path(current_user), success: 'プロフィールを登録しました'
+    else
+      flash.now[:danger] = 'プロフィールの作成に失敗しました'
+      render :new
+    end
   end
+
+  def show; end
 
   private
 
-  def set_user
-    @user = User.find(current_user.id)
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:dinner_time, :bedtime, day_of_week_ids: [])
   end
 end
