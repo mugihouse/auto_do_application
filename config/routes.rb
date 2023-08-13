257b72b6281a2fdfcb664f7c644fa4f3acfb52ed
mugihouse <mugi.stnk100122@gmail.com>
2023-08-13 19:51:48 +0900

    Add: タスク削除のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resource :profile, only: %i[new create show edit update]
-  resources :tasks, only: %i[new create edit update show index]
+  resources :tasks
 end
