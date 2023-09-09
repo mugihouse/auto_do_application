fc5321d8c07a9c12e6b7b881d5059b6274264258
mugihouse <mugi.stnk100122@gmail.com>
2023-09-09 22:57:33 +0900

    Add: enum設定追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :task
   belongs_to :user
 
+  enum status: { send_task: 0, done_task: 1 }
+  
   scope :today_send_messages, -> { where(delivery_date: DateTime.current.all_day )}
 end
