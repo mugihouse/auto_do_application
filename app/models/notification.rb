a09d996694fe6ff390620f85300652ac578fd101
mugihouse <mugi.stnk100122@gmail.com>
2023-09-10 19:38:21 +0900

    Fix: scopeに引数追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,5 +4,5 @@ class Notification < ApplicationRecord
 
   enum status: { send_task: 0, done_task: 1 }
 
-  scope :today_send_messages, -> { where(delivery_date: DateTime.current.all_day )}
+  scope :today_send_messages, -> (user_id){ where(delivery_date: DateTime.current.all_day, user_id: user_id)}
 end
