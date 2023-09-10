cfb73e258ed9c8b85a23f1b714fddb87a7f3d27e
mugihouse <mugi.stnk100122@gmail.com>
2023-09-10 14:40:36 +0900

    Add: Notificationモデルにtask_id: nilを許可

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ApplicationRecord
-  belongs_to :task
+  belongs_to :task, optional: true
   belongs_to :user
 
   enum status: { send_task: 0, done_task: 1 }
-  
+
   scope :today_send_messages, -> { where(delivery_date: DateTime.current.all_day )}
 end
