d913b188e13470beeefb828670676abd14998dfe
mugihouse <mugi.stnk100122@gmail.com>
2023-09-27 15:25:06 +0900

    Fix: migrationファイル書き換え

--- db/migrate/20230926224720_add_roles_to_users.rb
+++ db/migrate/20230926224720_add_roles_to_users.rb
@@ -1,9 +1,11 @@
 class AddRolesToUsers < ActiveRecord::Migration[7.0]
   def up
-    add_column :users, :role, :integer, null: false, default: 0
+    add_column :users, :role, :integer, default: 0
+    User.update_all(role: 0)
+    change_column_null :users, :role, false
   end
 
   def down
-    remove_column :users, :role, :integer
+    remove_column :users, :role, :integer, null: false
   end
 end
