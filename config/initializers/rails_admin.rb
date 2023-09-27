9f268bafb42057f9ad386bc3a3b3afe43b78a2aa
mugihouse <mugi.stnk100122@gmail.com>
2023-09-27 15:36:05 +0900

    Add: 認証設定追加

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,31 @@
+RailsAdmin.config do |config|
+  config.asset_source = :sprockets
+  config.parent_controller = 'ApplicationController'
+
+  ### Popular gems integration
+
+  config.authenticate_with do
+    user_id = session[:user_id]
+    user = User.find(user_id) if user_id
+    unless user && user.admin?
+      flash[:alert] = "権限がありません"
+      redirect_to main_app.root_path
+    end
+  end
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
