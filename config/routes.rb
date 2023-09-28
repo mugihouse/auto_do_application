8c0f42a176670715d169d136eef3d0c0c1734d77
mugi <114850902+mugihouse@users.noreply.github.com>
2023-09-28 23:22:56 +0900

    Fix: 不要なスペース削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
- Rails.application.routes.draw do
+Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#top'
   get '/after_login', to: 'static_pages#after_login'
