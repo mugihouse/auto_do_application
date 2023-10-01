d2d3b53b8b772e17bad3ab21487213c48d6aea69
mugihouse <mugi.stnk100122@gmail.com>
2023-10-01 19:58:48 +0900

    Change: contextの場合分け条件変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,24 +12,24 @@ RSpec.describe Task, type: :model do
   end
 
   describe '異常系テスト' do
-    context 'title' do
-      it '空の場合、保存できない' do
+    context 'titleがnil' do
+      it '保存できない' do
         task[:title] = nil
         task.valid?
         expect(task.errors.full_messages).to include('タイトルを入力してください')
       end
     end
 
-    context 'time_required' do
-      it '空の場合、保存できない' do
+    context 'time_requiredがnil' do
+      it '保存できない' do
         task[:time_required] = nil
         task.valid?
         expect(task.errors.full_messages).to include('所要時間を入力してください')
       end
     end
 
-    context 'user_id' do
-      it '空の場合、保存できない' do
+    context 'user_idがnil' do
+      it '保存できない' do
         task[:user_id] = nil
         task.valid?
         expect(task.errors.full_messages).to include('Userを入力してください')
