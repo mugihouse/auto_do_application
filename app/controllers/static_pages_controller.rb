3efe55e35ca04d34d93062342a5ebf8eb8eeb30a
mugihouse <mugi.stnk100122@gmail.com>
2023-10-18 00:08:18 +0900

    Add: chartに渡す変数追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def top
     if current_user
-      render :after_login
+      redirect_to :after_login
     else
       render :before_login
     end
@@ -9,9 +9,24 @@ class StaticPagesController < ApplicationController
 
   def before_login; end
 
-  def after_login; end
+  def after_login
+    @total = []
+    @notifications = Notification.all.where(user_id: current_user.id)
+    gon.total = this_week_task(current_user.id)
+  end
 
   def terms; end
 
   def privacy; end
+
+  private
+  def this_week_task(user_id)
+    @total.push(@notifications.done_6day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_5day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_4day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_3day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_2day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_1day_ago.map(&:get_time).sum)
+    @total.push(@notifications.done_today.map(&:get_time).sum)
+  end
 end
