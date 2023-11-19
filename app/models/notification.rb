46a80f931741c031e1068fe2fb60f045f6bb7dc4
mugihouse <mugi.stnk100122@gmail.com>
2023-11-19 23:31:18 +0900

    Fix: scopeを引数で管理する方法に変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,12 +11,7 @@ class Notification < ApplicationRecord
   scope :today_send_task, -> (user_id){ where(delivery_date: DateTime.current.all_day, status: 0, user_id: user_id) }
 
   scope :done_today, -> { where(created_at: DateTime.now.all_day) } # 今日
-  scope :done_1day_ago, -> { where(created_at: 1.day.ago.all_day) } # 前日
-  scope :done_2day_ago, -> { where(created_at: 2.days.ago.all_day) } # 2日前
-  scope :done_3day_ago, -> { where(created_at: 3.days.ago.all_day) } # 3日前
-  scope :done_4day_ago, -> { where(created_at: 4.days.ago.all_day) } # 4日前
-  scope :done_5day_ago, -> { where(created_at: 5.days.ago.all_day) } # 5日前
-  scope :done_6day_ago, -> { where(created_at: 6.days.ago.all_day) } # 6日前
+  scope :done_day_ago, -> (num){ where(created_at: num.day.ago.all_day) }
 
   def put_time
     task.time_required_before_type_cast
